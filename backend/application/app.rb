8acfd9c639a366a5ae58d215bf595902b9f3dbfa
Thiago do Nascimento <venancio862@gmail.com>
2024-08-18 21:57:22 -0300

    Modifying the code to use classes

--- backend/application/app.rb
+++ backend/application/app.rb
@@ -1,107 +1,116 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'redis'
 require 'json'
 
 require_relative 'utils/crypt_manager'
 
-redis = Redis.new(host: 'redis', port: ENV.fetch('REDIS_PORT', 6379))
-crypt_manager = CryptManager.new
 
-before do
-  headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
-          'Access-Control-Allow-Headers' => 'Authorization, Content-Type'
-end
+class SecretMessages < Sinatra::Base
 
-options '*' do
-  response.headers['Allow'] = 'GET, POST, PUT, DELETE, OPTIONS, PATCH'
-  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, Origin'
-  response.headers['Access-Control-Allow-Origin'] = '*'
-  200
-end
+  set :port, ENV['SINATRA_PORT']
+  use Rack::RewindableInput::Middleware
 
-
-
-get '/' do
-  "<h1>Yeah, it's apparently working, my friend! xD :) :p =)</h1>"
-end
-
-
-
-post '/store' do
-  request.body.rewind
-  begin
-    data = JSON.parse(request.body.read)
-  rescue JSON::ParserError => e
-    halt 400, { message: 'An invalid JSON was sent!' }.to_json
+  def initialize
+    super()
+    @redis = Redis.new(host: 'redis', port: ENV.fetch('REDIS_PORT', 6379))
+    @crypt_manager = CryptManager.new
   end
 
-  if !data.key?("message")
-    halt 400, { message: 'The message is missing!' }.to_json
+  before do
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+            'Access-Control-Allow-Headers' => 'Authorization, Content-Type'
   end
 
-  if !data.key?("password")
-    halt 400, { message: 'The password is missing!' }.to_json
+  options '*' do
+    response.headers['Allow'] = 'GET, POST, PUT, DELETE, OPTIONS, PATCH'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, Origin'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    200
   end
 
-  message = data['message']
-  password = data['password']
 
-  if !message || message == "" || !password || password == ""
-    halt 400, { message: 'The message or the password can\'t be empty!' }.to_json
+
+  get '/' do
+    "<h1>Yeah, it's apparently working, my friend! xD :) :p =)</h1>"
   end
 
-  key = SecureRandom.hex(16)
-
-  aes_key = crypt_manager.derive_aes_key(password, key)
-
-  message = crypt_manager.encrypt(message, aes_key)
-
-  redis.set(key, message)
-  
-  { key: key }.to_json
-end
 
 
-
-patch '/retrieve' do
-  request.body.rewind
-  begin
-    data = JSON.parse(request.body.read)
-  rescue JSON::ParserError => e
-    halt 400, { message: 'An invalid JSON was sent!' }.to_json
-  end
-
-  if !data.key?("key")
-    halt 400, { message: 'The key is missing!' }.to_json
-  end
-
-  if !data.key?("password")
-    halt 400, { message: 'The password is missing!' }.to_json
-  end
-
-  key = data['key']
-  password = data['password']
-
-  message = redis.get(key)
-
-  if !message
-    status 404
-
-    { message: "Sorry, the message wasn't found!" }.to_json
-  else
-    aes_key = crypt_manager.derive_aes_key(password, key)
-
+  post '/store' do
+    request.body.rewind
     begin
-      decrypted = crypt_manager.decrypt(message, aes_key)
-    rescue OpenSSL::Cipher::CipherError => e
-      halt 400, { message: 'Invalid password sent!' }.to_json
+      data = JSON.parse(request.body.read)
+    rescue JSON::ParserError => e
+      halt 400, { message: 'An invalid JSON was sent!' }.to_json
     end
 
-    redis.del(key)
+    if !data.key?("message")
+      halt 400, {message: 'The message is missing!'}.to_json
+    end
 
-    { message: decrypted }.to_json
+    if !data.key?("password")
+      halt 400, {message: 'The password is missing!'}.to_json
+    end
+
+    message = data['message']
+    password = data['password']
+
+    if !message || message == "" || !password || password == ""
+      halt 400, {message: 'The message or the password can\'t be empty!'}.to_json
+    end
+
+    key = SecureRandom.hex(16)
+
+    aes_key = @crypt_manager.derive_aes_key(password, key)
+
+    message = @crypt_manager.encrypt(message, aes_key)
+
+    @redis.set(key, message)
+
+    { key: key }.to_json
   end
-end
 
-set :port, ENV['SINATRA_PORT']
+
+
+  patch '/retrieve' do
+    request.body.rewind
+    begin
+      data = JSON.parse(request.body.read)
+    rescue JSON::ParserError => e
+      halt 400, { message: 'An invalid JSON was sent!' }.to_json
+    end
+
+    if !data.key?("key")
+      halt 400, { message: 'The key is missing!' }.to_json
+    end
+
+    if !data.key?("password")
+      halt 400, { message: 'The password is missing!' }.to_json
+    end
+
+    key = data['key']
+    password = data['password']
+
+    message = @redis.get(key)
+
+    if !message
+      status 404
+
+      { message: "Sorry, the message wasn't found!" }.to_json
+    else
+      aes_key = @crypt_manager.derive_aes_key(password, key)
+
+      begin
+        decrypted = @crypt_manager.decrypt(message, aes_key)
+      rescue OpenSSL::Cipher::CipherError => e
+        halt 400, { message: 'Invalid password sent!' }.to_json
+      end
+
+      @redis.del(key)
+
+      { message: decrypted }.to_json
+    end
+  end
+
+end
---
+++ backend/application/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rackup -o 0.0.0.0 -p ${SINATRA_PORT}
